28c4e3c87f8c317ab40887be308d12965b217793
ceramicskate0 <64501203+ceramic-skate0@users.noreply.github.com>
2023-02-13 14:31:34 -0500

    Add files via upload

---
+++ FindAndReplace.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Ceramic
+{
+    class FindAndReplace
+    {
+        public static void ReadFileReplaceString(string InputFilePath, string FindThis, string ReplaceItWithThis)
+        {
+            if (!File.Exists(InputFilePath))
+            {
+                Console.WriteLine(InputFilePath + " Does not exist.");
+                Environment.Exit(1);
+            }
+            if (File.Exists(ReplaceItWithThis))
+            {
+                ReplaceItWithThis = File.ReadAllText(ReplaceItWithThis);
+            }
+            string FileContents = File.ReadAllText(InputFilePath);
+            var regex = new Regex(FileContents);
+            FileContents = regex.Replace(FindThis, ReplaceItWithThis, 1);
+            File.WriteAllText(InputFilePath, FileContents);
+            Console.WriteLine("Replaced contents of file with what you wanted if it was there.");
+        }
+    }
+}
